3102d9baebbf1c58e69af6c0a88bcb1d2e6b2cdd
ayoub-hafidi-alaoui <ayoub.hafidi.alaoui@hotmail.fr>
2022-03-26 18:43:03 +0100

    add clients and social media

--- handlers/DB.php
+++ handlers/DB.php
@@ -1,10 +1,37 @@
 <?php
 require_once('connect.php');
-function getAll($table) {
+function getAll($table)
+{
     global $conn;
     $query = "SELECT * FROM $table";
     $getAll = mysqli_query($conn, $query);
     $getAllData = mysqli_fetch_all($getAll, MYSQLI_ASSOC);
     return $getAllData;
 }
+
+function getWhere($table, $where)
+{
+    global $conn;
+    $query = "SELECT * FROM $table WHERE $where";
+    $getAll = mysqli_query($conn, $query);
+    $getAllData = mysqli_fetch_all($getAll, MYSQLI_ASSOC);
+    return $getAllData;
+}
+function getJoin($table1, $table2, $col,$primary_key, $foreign_key) {
+    global $conn;
+    $query = "SELECT $col FROM $table1 INNER JOIN $table2 on $table1.$primary_key=$table2.$foreign_key";
+    $getAll = mysqli_query($conn, $query);
+    $getAllData = mysqli_fetch_all($getAll, MYSQLI_ASSOC);
+    return $getAllData;
+
+}
+
+function getOnes($table, $where) {
+    global $conn;
+    $query= "SELECT * FROM $table WHERE $where";
+    $getAll = mysqli_query($conn, $query);
+    $getData = mysqli_fetch_assoc($getAll);
+    return $getData;
+}
+
 ?>
